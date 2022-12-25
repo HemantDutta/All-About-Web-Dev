0c6e131a488db0f9b3f03b05377fcd63726f56a4
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-26 00:16:03 +0530

    express server creation

---
+++ BackendDevelopment/ExpressJS/server.js
@@ -0,0 +1,15 @@
+//First install express, npm install express
+//Now Require Express
+const express = require('express');
+
+//Declare a constant express app
+const app = express();
+
+//Now add a post to listen on
+const port = 3000;
+
+//The function passed here will be executed when the server starts running successfully!
+app.listen(port, function (){
+    console.log("Server is running on port 3000");
+});
+
