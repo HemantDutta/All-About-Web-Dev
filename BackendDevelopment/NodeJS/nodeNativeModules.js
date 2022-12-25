9b5b865ae8e3661037ea5d5641afe2bf4f2fae38
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-25 23:18:34 +0530

    fs.copyFileSync

--- BackendDevelopment/NodeJS/nodeNativeModules.js
+++ BackendDevelopment/NodeJS/nodeNativeModules.js
@@ -1,1 +1,7 @@
+//Importing native node module fileSystem
 const fs = require("fs");
+
+//Importing file into project, copying source file into a new file
+fs.copyFileSync('file1.txt', 'file2.txt');
+//Before execution, make sure your terminal's path is set to the directory where your source file resides
+//After execution, a new 'file2.txt' will be created with the same content as file1.txt, if file2.txt already exists, it will be overwritten
