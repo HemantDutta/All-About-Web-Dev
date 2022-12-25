b038e4cd5a584d9b1be50506dac980e5dd886a6e
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-25 23:37:26 +0530

    random Words Package

---
+++ BackendDevelopment/NodeJS/npm.js
@@ -0,0 +1,16 @@
+//Importing an external package
+//Make sure to first install it using npm install or npm i
+const superheroes = require("superheroes");
+let shName = superheroes.random();
+console.log(shName);
+
+//Now, I'll try to install another package and try it out
+//I will use random-words packages
+//I will first install it using npm i random-words
+//Now, we will import it
+const rw = require("random-words");
+
+let wordString = rw({exactly: 100, join: ' '});
+console.log(wordString);
+
+//This is exactly what I needed ;)
