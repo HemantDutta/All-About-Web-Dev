0220f7e3349b7dbcc7a2873bb0bd46833287a289
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-26 17:22:26 +0530

    api json parsing and writing

--- APIs/BoredAPI/server.js
+++ APIs/BoredAPI/server.js
@@ -2,9 +2,13 @@
 //Second, Install Express
 //Now, Import Express
 const express = require('express');
+const https = require('https');
 
 //Now, create an express app
 const app = express();
+
+//Mention Directory where resources like stylesheets and scripts are
+// app.use(express.static(__dirname + "/"));
 
 //Establish a port where the server will listen
 const port = 3003;
@@ -15,4 +19,23 @@ app.listen(port, ()=>{
 //Get Request Handling
 app.get("/", (req,res)=>{
 
+    // res.sendFile(__dirname + "/index.html");
+
+    //Fetch API Data
+    const url = "https://www.boredapi.com/api/activity/";
+    https.get(url, (resp)=>{
+        resp.on("data", (data)=>{
+            let act = JSON.parse(data)
+            console.log(act);
+            let actName = act.activity;
+            let actPart = act.participants;
+            let actPrice = act.price;
+
+            //We can only use on res.send in a GET request, but we can use multiple res.write() requests and res.send() at the end to send all the written text
+            res.write('<h1>Activity Name: ' + actName);
+            res.write('<h1>Participants needed: ' + actPart);
+            res.write('<h1>Price: $' + actPrice);
+            res.send();
+        });
+    });
 });
